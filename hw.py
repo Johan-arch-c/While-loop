7d4da19601bfee52dbdd2c44be9d0e6cbd65f066
Johan-arch-c <johannithya@gmail.com>
2025-08-06 17:55:38 +1000

    hw

--- hw.py
+++ hw.py
@@ -1,7 +1,6 @@
-n=int(input("enter the value of term:"))
-sum=0
-i=2
-while i<=n:
-    sum=sum+i
-    i=i+1
-print("\n Sum=", sum)
+my_list = [1, 2, 3, 4]
+my_list.reverse()
+print(my_list)
+my_list = [4, 3, 2, 1]
+reversed_list = my_list[::-1]
+print(reversed_list)
